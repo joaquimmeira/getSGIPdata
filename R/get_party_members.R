e6ff61bc87519aaafeb86bf982dfa400d2d86d12
SamuelBRodrigues <sam.baraque@gmail.com>
2025-02-22 09:52:30 -0300

    update fucntion restructure

--- R/get_party_members.R
+++ R/get_party_members.R
@@ -8,44 +8,45 @@
 #' @return A tibble containing the members of the party organization.
 #' @export
 #'
-#' @importFrom stringr str_glue
-#' @importFrom jsonlite read_json
-#' @importFrom dplyr tibble
-#' @importFrom tidyr unnest_wider
-#'
 #' @examples
 #' \dontrun{
-#'   members <- get_party_members('12345')
+#'   members <- get_party_members(12345)
 #'   print(members)
 #' }
 get_party_members <- function(id_orgao_partidario) {
   # Input validation
-  if (!is.character(id_orgao_partidario) || length(id_orgao_partidario) != 1) {
-    stop("The parameter 'id_orgao_partidario' must be a  numeric character (integer)")
+  if (!is.character(id_orgao_partidario)) {
+    stop("The parameter 'id_orgao_partidario' must be a character vector.")
   }
+  members_list <- purrr::map_df(
+    id_orgao_partidario,
+    ~ {
 
-  # Build the URL
-  request_url <- stringr::str_glue(
-    "https://sgip3.tse.jus.br/sgip3-consulta/api/v1/orgaoPartidario/comAnotacoesEMembros?idOrgaoPartidario={id_orgao_partidario}"
-  )
+      Sys.sleep(runif(1, 1, 3))
 
-  # Error handling for the API request
-  tryCatch({
-    response <- jsonlite::read_json(request_url)
+      # Build the URL
+      req <- request("https://sgip3.tse.jus.br/sgip3-consulta/api/v1/orgaoPartidario/comAnotacoesEMembros") |>
+        req_url_query(idOrgaoPartidario = .x)
 
-    # Check if the response contains the 'membros' key
-    if (!"membros" %in% names(response)) {
-      stop("The API response does not contain the 'membros' key.")
+      response <- httr2::req_perform(req) |> httr2::resp_body_json()
+
+      # Check if the response contains the 'membros' key
+      if (!"membros" %in% names(response)) {
+        stop("The API response does not contain the 'membros' key.")
+      }
+
+      # Convert the list of members into a tibble and unnest the columns
+      members <- response$membros |>
+        tibble::tibble() |>
+        tidyr::unnest_wider(dplyr::everything()) |>
+        dplyr::mutate(
+          party_id = .x
+        )
+
+
+      members |> dplyr::glimpse()
+
+      return(members)
     }
-
-    # Convert the list of members into a tibble and unnest the columns
-    members <- response$membros |>
-      dplyr::tibble(data = _) |>
-      tidyr::unnest_wider(data)
-
-    return(members)
-
-  }, error = function(e) {
-    stop("Error accessing the API: ", e$message)
-  })
+  )
 }
